9ace85a3f1baa9bbacc3af0288503c783a4e2de6
kkarpouz <karpouzas@astro.rug.nl>
2019-10-06 23:55:50 +0200

    php_form_test

--- example.php
+++ example.php
@@ -1,10 +1,8 @@
-<!DOCTYPE html>
 <html>
 <body>
 
-<?php
-echo "My first PHP script!";
-?> 
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
 
 </body>
 </html>
